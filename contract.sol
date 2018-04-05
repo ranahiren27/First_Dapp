0cfbc70e9e46fb7e2482580f732722ef05b09969
ranahiren <ranahiren27@gmail.com>
2018-04-06 00:05:23 +0530

    commiting

---
+++ contract.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.10;
+
+
+
+contract Owned {
+    address owner;
+    
+    function Owned() public{
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+}
+
+
+
+contract Cources is Owned{
+    struct Instructor{
+        uint age;
+        bytes16 fname;
+        bytes16 lname;
+    }
+    
+    mapping (address => Instructor) instructors;
+    address[] public instructorAccts;
+    
+    event instructorInfo(
+        bytes16 fname,
+        bytes16 lname,
+        uint age
+    );
+    
+    function setInstructor(address _address, uint _age, bytes16 _fname, bytes16 _lname) onlyOwner public{
+        var instructor = instructors[_address];
+        
+        instructor.age = _age;
+        instructor.fname = _fname;
+        instructor.lname = _lname;
+        
+        instructorAccts.push(_address) -1;
+        instructorInfo(_fname,_lname,_age);
+        
+    }
+    
+    function getInstructors() view public returns(address[]){
+        return instructorAccts;
+    }
+    
+    function getInstructor(address _address) view public returns(uint,bytes16,bytes16){
+        return (instructors[_address].age,instructors[_address].fname,instructors[_address].lname);
+    } 
+    
+    function countInstructor() view public returns(uint){
+        return instructorAccts.length;
+    }
+}
